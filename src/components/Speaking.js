003b06db3fac96fb81c4232188bec3699e2d28ca
Heidi Olsen <heidi@eroi.com>
2018-06-17 15:24:59 -0700

    Update speaker card pattern

--- src/components/Speaking.js
+++ src/components/Speaking.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import conferences from '../data/conferences.json';
-import webinars from '../data/webinars.json';
-import meetups from '../data/meetups.json';
+import talks from '../data/talks.json';
 import '../css/Speaking.css';
 
 class Speaking extends React.Component {
   render() {
+    const speakerGrid = talks.map((talk) =>
+      <div key={talk.id} className="speaker__boxx">
+        <div class="border"></div>
+        <h4>{talk.category}</h4>
+        <h3><a href={talk.conference_url} target="_blank">{talk.title}</a></h3>
+        <p>{talk.date}</p>
+        <p>{talk.location}</p>
+        {talk.cta ? <button href={talk.cta} target="_blank">{talk.cta_text}</button> : "" }
+      </div>
+    );
     return (
       <section className="speaking" id="speaking">
-      <h2>Speaking</h2>
-        <div className="speaking-container">
-          <h3>Conferences</h3>
-          <ul>  
-             { conferences.map(function(conferences) {
-              return <li>
-                <h3>{conferences.title}</h3>
-                <p>{conferences.date}</p>
-                <p>{conferences.desc}</p>
-                <p><a href="{conferences.url" target="_blank">View Slides</a></p>
-              </li>;
-              })
-            }
-          </ul>
-        </div>
-        <div className="speaking-container">
-          <h3>Webinars</h3>
-          <ul>  
-             { webinars.map(function(webinars) {
-              return <li>
-                <h3>{webinars.title}</h3>
-                <p>{webinars.date}</p>
-                <p>{webinars.desc}</p>
-                <p><a href="{webinars.url}" target="_blank">View Slides</a></p>
-              </li>;
-              })
-            }
-          </ul>
-        </div>
-        <div className="speaking-container">
-          <h3>Meetups</h3>
-          <ul>  
-             { meetups.map(function(meetups) {
-              return <li>
-                <h3>{meetups.title}</h3>
-                <p>{meetups.date}</p>
-                <p>{meetups.desc}</p>
-                <p><a href="{meetups.url}" target="_blank">View Slides</a></p>
-              </li>;
-              })
-            }
-          </ul>
+        <h2>Speaking</h2>
+        <div className="speaker__grid">
+          {speakerGrid}
         </div>
       </section>
     );
@@ -57,5 +26,3 @@ class Speaking extends React.Component {
 }
 
 export default Speaking;
-
-
